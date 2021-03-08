12a14d166ce2d5b99d45265e7013458b1156fe54
XZETGEXI <77068772+XZETGEXI@users.noreply.github.com>
2021-03-08 11:11:07 +0100

    no longer requires external libraries
    Used Unicode chars instead of kanji-lists so the script could run immediately

--- bobabiler.py
+++ bobabiler.py
@@ -1,16 +1,21 @@
 from random import random, choice, choices, randint, shuffle
 
-from kanji_lists import JOYO
-
-import os, time, math
+import os, time, math, string
 
 W, H = os.get_terminal_size()
 
+MSG_TIMING = 4
+POSTER_TIMING = 4
+NAME_LEN = 6
+MSG_LEN = W // 2 - 4
+TEXT_WHITESPACES = 8
+DOT_ENDING_PROBABILITY = 10
+MAX_SENTENCES = 5
+
+
 NUMBERS = [f'{i}' for i in range(10)]
+END_CHARACTERS = ["?", " !", "...", " ^_^"] + ["."] * DOT_ENDING_PROBABILITY
 
-CHARACTERS = [*JOYO]
-
-END_CHARACTERS = ["?", " !", "...", " ^_^"] + ["."] * 10
 
 def rad_rand(n = 1):
     assert n <= 18014398509481982
@@ -21,49 +26,72 @@ def rad_rand(n = 1):
         return 0
 
 
-def main():
+def get_dateclock():
+    dates = [rad_rand(31), rad_rand(12), 2000 + rad_rand(21)]
+    dates = [str(date) for date in dates]
+    date = "/".join(dates)
+
+    clocks = [rad_rand(24) - 1, rad_rand(60 - 1)]
+    clocks = [str(clock) for clock in clocks]
+    clock = ":".join(clocks)
+        
+    dateclock = date + " | " + clock
+    return dateclock
+
+
+def get_name():
+    name_length = rad_rand(NAME_LEN)
+
+    name = []
+    for i in range(name_length):
+        name.append(chr(rad_rand(200) + 12036))
+
+    name = "".join(name)
+    name = "[" + name + "]"
     
+    return name
+    
+    
+def get_msg():
+    msg_length = rad_rand(MSG_LEN)
+
+    msg = []
+    for i in range(msg_length):
+        msg.append(chr(rad_rand(200) + 12036))
+            
+    msg += [" "] * rad_rand(TEXT_WHITESPACES)
+    shuffle(msg)
+    msg = "".join(msg)
+    msg = msg.strip()
+    msg = "> " + msg
+    msg = msg + choice(END_CHARACTERS)
+        
+    return msg
+
+
+def pause(timing):
+    sleep_time = rad_rand(MSG_TIMING)
+    time.sleep(math.sqrt(sleep_time))
+
+
+def main():
     while True:
         
-        dates = [rad_rand(31), rad_rand(12), 2000 + rad_rand(21)]
-        dates = [str(date) for date in dates]
-        date = "/".join(dates)
-        
-        clocks = [rad_rand(25) - 1, rad_rand(60 - 1)]
-        clocks = [str(clock) for clock in clocks]
-        clock = ":".join(clocks)
-        
-        dateclock = date + " | " + clock
-        
+        dateclock = get_dateclock()
         print(dateclock.rjust(W, " "))
         
-        name_length = rad_rand(6)
-        name = "".join(choices(CHARACTERS, k = name_length))
-        name = "[" + name + "]"
-        
+        name = get_name()
         print(name)
         
-        sentences = rad_rand(5)
+        sentences = rad_rand(MAX_SENTENCES)
         for sentence in range(sentences):
-            
-            msg_length = rad_rand(40)
-            msg = choices(CHARACTERS, k = msg_length)
-            msg += [" "] * rad_rand(4)
-            shuffle(msg)
-            msg = "".join(msg)
-            msg = msg.strip()
-            msg = "> " + msg
-            msg = msg + choice(END_CHARACTERS)
-            
+            msg = get_msg()
             print(msg)
+            
+            pause(MSG_TIMING)
+            
+        pause(POSTER_TIMING)
         
-            sleep_time = rad_rand(4)
-            time.sleep(0.1 * sleep_time)
-    
-        sleep_time = rad_rand(4)
-        time.sleep(math.log(1.1 * sleep_time))
         
-        print()
 
-      
 main()
